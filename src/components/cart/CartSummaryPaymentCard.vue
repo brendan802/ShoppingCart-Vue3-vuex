7f476c001978ea736911045e43fd9746dfda0155
brendan <brendan@brendans-MacBook-Pro.local>
2021-11-12 16:01:27 -0500

    commit 2

---
+++ src/components/cart/CartSummaryPaymentCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="cart-item-card header">
+        <h3>Cart Total: ${{ cart_total.toFixed(2) }}</h3>
+        <button class="view-product-button"> Pay By Snail Mail</button>
+    </div>
+</template>
+
+<script>
+
+    export default{
+     
+
+        computed:{
+            cart_total () {
+                return this.$store.getters.cartTotal
+            }
+        }
+    }
+</script>
---
+++ src/data/items.js
@@ -0,0 +1,84 @@
+const items = [ 
+    {
+        id:1,
+        name:'Lamson Smoke',
+        price: 175.00,
+        category: 'Reel',
+        description: 'Suspendisse risus nisi, bibendum quis lorem id, mollis porttitor dui. Aenean ut suscipit sem, sagittis sollicitudin felis. Mauris consequat leo pharetra, hendrerit mi id, sodales leo. Quisque ut sodales urna. '
+
+    },
+
+    {
+        id:2,
+        name:'Orvis Guide',
+        price: 695.00,
+        category: 'Waders',
+        description: 'Suspendisse risus nisi, bibendum quis lorem id, mollis porttitor dui. Aenean ut suscipit sem, sagittis sollicitudin felis. Mauris consequat leo pharetra, hendrerit mi id, sodales leo. Quisque ut sodales urna. '
+
+    },
+
+    {
+        id:3,
+        name:'Clearwater 8WT',
+        price: 345.00,
+        category: 'Rod',
+        description: 'Suspendisse risus nisi, bibendum quis lorem id, mollis porttitor dui. Aenean ut suscipit sem, sagittis sollicitudin felis. Mauris consequat leo pharetra, hendrerit mi id, sodales leo. Quisque ut sodales urna. '
+
+    },
+
+    {
+        id:4,
+        name:'Lamson 9WT',
+        price: 455.00,
+        category: 'Rod',
+        description: 'Suspendisse risus nisi, bibendum quis lorem id, mollis porttitor dui. Aenean ut suscipit sem, sagittis sollicitudin felis. Mauris consequat leo pharetra, hendrerit mi id, sodales leo. Quisque ut sodales urna. '
+
+    },
+
+    {
+        id:5,
+        name:'Simms Tributary',
+        price: 179.00,
+        category: 'Waders',
+        description: 'Suspendisse risus nisi, bibendum quis lorem id, mollis porttitor dui. Aenean ut suscipit sem, sagittis sollicitudin felis. Mauris consequat leo pharetra, hendrerit mi id, sodales leo. Quisque ut sodales urna. '
+
+    },
+
+
+    {
+        id:6,
+        name:'St. Croix Imperial ',
+        price: 299.00,
+        category: 'Rod',
+        description: 'Suspendisse risus nisi, bibendum quis lorem id, mollis porttitor dui. Aenean ut suscipit sem, sagittis sollicitudin felis. Mauris consequat leo pharetra, hendrerit mi id, sodales leo. Quisque ut sodales urna. '
+
+    },
+    {
+        id:7,
+        name:'Douglas DXF ',
+        price: 445.00,
+        category: 'Rod',
+        description: 'Suspendisse risus nisi, bibendum quis lorem id, mollis porttitor dui. Aenean ut suscipit sem, sagittis sollicitudin felis. Mauris consequat leo pharetra, hendrerit mi id, sodales leo. Quisque ut sodales urna. '
+
+    },
+    {
+        id:8,
+        name:'Ross Evolution ',
+        price: 395.00,
+        category: 'Reel',
+        description: 'Suspendisse risus nisi, bibendum quis lorem id, mollis porttitor dui. Aenean ut suscipit sem, sagittis sollicitudin felis. Mauris consequat leo pharetra, hendrerit mi id, sodales leo. Quisque ut sodales urna. '
+
+    },
+    {
+        id:9,
+        name:'Ross Colorado',
+        price: 370.00,
+        category: 'Reel',
+        description: 'Suspendisse risus nisi, bibendum quis lorem id, mollis porttitor dui. Aenean ut suscipit sem, sagittis sollicitudin felis. Mauris consequat leo pharetra, hendrerit mi id, sodales leo. Quisque ut sodales urna. '
+
+    },
+
+
+]
+
+export default items 

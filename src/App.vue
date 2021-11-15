3cbbf26d0f0b0ef3716c5b365311bcd5381ac7b8
brendan <brendan@brendans-MacBook-Pro.local>
2021-11-15 13:23:46 -0500

    readme gone

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link :class="{active: $route.name === 'Home'}" to="/">Home</router-link> 
-    <router-link :class="{active: $route.name === 'Cart'}" to="/cart">About</router-link>
+    <router-link :class="{active: $route.name === 'Cart'}" to="/cart">Cart</router-link>
   </div>
   <router-view/>
 </template>
